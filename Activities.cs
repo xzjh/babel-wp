d32436938607e22e48174bf7daba948f7eb8e488
Jiaheng Zhang <jsxzjh@gmail.com>
2015-01-31 11:54:54 -0800

    ~

---
+++ Activities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Windows;
+namespace Babel
+{
+    class Activities
+    {
+        public ObservableCollection<Activity> ActivitiesList { get; set; }
+    }
+}
---
+++ ResponseParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel
+{
+    class ResponseParser
+    {
+        private Dictionary<string, string> parseResponse(string parameters)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            if (!string.IsNullOrEmpty(parameters))
+            {
+                string[] p = parameters.Split('&');
+                foreach (string s in p)
+                    if (!string.IsNullOrEmpty(s))
+                        if (s.IndexOf('=') > -1)
+                        {
+                            string[] temp = s.Split('=');
+                            result.Add(temp[0], temp[1]);
+                        }
+                        else result.Add(s, string.Empty);
+            }
+
+            return result;
+        }
+    }
+}
