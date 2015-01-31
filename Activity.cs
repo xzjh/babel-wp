fca600d6846c2710ff465901fbeb240d6525ab45
tzhong92 <tzheecspku@gmail.com>
2015-01-31 00:56:12 -0800

    add message page

---
+++ Activity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel
+{
+    class Activity
+    {
+        public string ActivityId { get; set; }
+        public string ActivityTitle { get; set; }
+        public DateTime ActivityTime { get; set; }
+        public int Capacity {get; set;}
+        public int Available { get; set;}
+        public int Budget { get; set; }//0 for free; 1 for 0-10; 2 for 10-20; 3 for 20 - 50; 4 for over 50; 5 for Not sure
+        public int LocationIndex { get; set; }//
+        public string LocationDetail { get; set; }
+        public string Destination { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Babel
+{
+    class User
+    {
+        public string UserName { get; set; }
+        public string UserIconUrl { get; set; }
+        public string UserFacebookUrl { get; set; }
+        public string UserId { get; set; }
+        public string UserIntroduction { get; set; }
+        public string UserDescription { get; set; }
+    }
+}
